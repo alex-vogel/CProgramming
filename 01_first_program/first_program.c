ba1aad3b3a85e1a1a0f1451f95e33724b79c1c35
alex-vogel <avogel7654@floridapoly.edu>
2025-01-16 16:29:53 +0000

    First program written in class

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,19 @@
+// One-line comment
+// This is the first program which prints a welcoming message.
+/*
+this is a multi line comment
+
+January 9th 2025.
+
+
+*/
+
+#include <stdio.h>
+
+// Create a main function
+int main() {
+    printf("Welcome\nto\nCOP2271!\n");
+    printf("I\nwant\nto\ncode\n");
+
+    return 0;
+}
