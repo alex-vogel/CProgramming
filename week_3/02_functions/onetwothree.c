be932270b695f8e189e1ec14ef8f389743dbf5d9
alex-vogel <avogel7654@floridapoly.edu>
2025-01-28 16:21:09 +0000

    updated one two three program with prototype functions

--- week_3/02_functions/onetwothree.c
+++ week_3/02_functions/onetwothree.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-void two(void) {
-    printf("two\n");
+void two(void);
+void one_three(void);
+
+int main() {
+    printf("starting now:\n");
+    one_three();
+    printf("done!\n");
 }
 
 void one_three(void) {
@@ -10,9 +15,12 @@ void one_three(void) {
     printf("three\n");
 }
 
-
-int main() {
-    printf("starting now:\n");
-    one_three();
-    printf("done!\n");
+void two(void) {
+    printf("two\n");
 }
+
+
+
+// note you can create function prototypes just using the header. This allows you
+//to put the function somewhere under where youre calling it from if you want.
+//generally you want main at the top of your code
